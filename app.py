a195818d0c41b26fc1d6b8d5b85751364cb09e7f
a-domagalski <a.domagalski96@gmail.com>
2024-09-15 15:10:41 +0200

    Add migrations and styles

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config.Config')  # Load config from config.py
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Import routes after initializing app and db
 from routes import *
--- run.bat
+++
@@ -1,2 +0,0 @@
-D:/!studia/mgr/IIsem/python_classes/jira_ish/venv/Scripts/python.exe app.py
-
